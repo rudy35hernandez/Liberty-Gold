a061fbd917888c6f7ac78b46c95f04220ce5edb3
rudy35hernandez <rudy35hernandez@gmail.com>
2024-12-01 23:38:27 -0800

    fixed submission for form data

--- public/index.js
+++ public/index.js
@@ -9,14 +9,33 @@
     });
 
 
-    document.addEventListener("DOMContentLoaded", function(){
-        const form = document.getElementById("consultation-form")
-        const thankYouMessage = document.getElementById("thank-you-message")
+    document.getElementById("consultation-form").addEventListener("submit", async function(event){
+        event.preventDefault()
 
-        form.addEventListener("submit", function(event) {
-            event.preventDefault()
+        const formData = FormData(event.target) // form data from form
+        const data = Object.fromEntries(formData.entries())
 
-            form.style.display = "none"
-            thankYouMessage.style.display = "block"
-        })
+        try {
+            const response = await fetch("/submit-form", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application.json",
+                },
+                body: JSON.stringify(data),
+            });
+
+            if(response.ok){
+                //update page with thank you message
+                document.querySelector(".form-section").innerHTML = `
+                <h2> Thank you for reaching out!</h2> 
+                <p> We will contact you shortly.</p>
+                `
+            } else {
+                throw new Error("Failed to submit form");
+            } 
+        }  catch (error) {
+            console.error("Error submitting the form", error);
+            alert("Something went wrong. Please try again later.")
+        }
     })
+        
